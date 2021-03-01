f44a540d87c41c581970c79040e72217f260ee70
brunohca <42281349+brunohca@users.noreply.github.com>
2021-03-01 17:02:14 -0300

    Create 1 - Create schemas.sql

---
+++ 2) - Setup - Database sripts/1 - Create schemas.sql
@@ -0,0 +1,14 @@
+--1) Start dedicated SQL Pool
+
+--2) Connect your Azure Data Studio, Management Studio or select develop tab in your Synapse workspace
+
+--3) Execute below scripts:
+
+CREATE SCHEMA operation;
+GO
+
+CREATE SCHEMA stage;
+GO
+
+CREATE SCHEMA dw;
+GO
